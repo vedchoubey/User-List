66a85b48a5bd393d209a4195ae6d4b6c5738e3d6
vedchoubey <147600515+vedchoubey@users.noreply.github.com>
2024-10-21 12:52:27 +0530

    created a typescript project with react

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Abc } from "./components/abc";
+
+export const App = () => {
+  return(
+    <>
+    <Abc />
+    </>
+  )
+}
---
+++ src/components/abc.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+export const Abc = () => {
+    return(
+        <>
+        <Typography>vvv</Typography>
+        </>
+    )
+}
